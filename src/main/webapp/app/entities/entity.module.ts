7983668304c6cfd6a28eef6479ceca24150e5243
Radu Gheorghescu <radugamingtv@gmail.com>
2019-01-13 18:08:43 +0200

    Done

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,22 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { BlogCategoryModule } from './category/category.module';
+import { BlogEntryModule } from './entry/entry.module';
+import { BlogTagModule } from './tag/tag.module';
+import { BlogProductCategoryModule as ShopProductCategoryModule } from './shop/product-category/product-category.module';
+import { BlogProductModule as ShopProductModule } from './shop/product/product.module';
+import { BlogReviewModule as ShopReviewModule } from './shop/review/review.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        BlogCategoryModule,
+        BlogEntryModule,
+        BlogTagModule,
+        ShopProductCategoryModule,
+        ShopProductModule,
+        ShopReviewModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
